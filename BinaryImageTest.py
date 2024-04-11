4146406afc3fac30dda76b84b9a8bef36c182e57
Tristen Miller <millert9999@gmail.com>
2024-04-11 11:48:44 -0700

    Have masking via height and depth working conceptually

--- BinaryImageTest.py
+++ BinaryImageTest.py
@@ -8,8 +8,15 @@ if __name__ == "__main__":
     while (1):
         color,depth = camera.frames()
         color_im = ColorImage(color)
-        bin_im = color_im.foreground_mask(50,ignore_black =False )
+        depth_im = DepthImage(depth)
+        filtered_depth_im = depth_im.threshold(.5,1)
+
+        bin_im = color_im.foreground_mask(50,ignore_black =False)
+        depth_mask = filtered_depth_im.invalid_pixel_mask()
+        right_depth_mask = depth_mask.inverse()
+        final_obj_mask = bin_im.mask_binary(right_depth_mask)
         cv.imshow("color",color)
-        cv.imshow("depth",depth)
-        cv.imshow("binary",bin_im._image_data())
+        cv.imshow("obj_mask",final_obj_mask._image_data())
+        #cv.imshow("depth_filtered",filtered_depth_im._image_data())
+        #cv.imshow("binary",bin_im._image_data())
         cv.waitKey(1)
