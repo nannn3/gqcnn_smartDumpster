e3441459a6c62aeb3ef93b09b9f7f65b2666c9e0
nannn3 <millert9999@gmail.com>
2024-04-25 11:52:00 -0700

    Clean-up

--- main.py
+++ main.py
@@ -2,88 +2,84 @@ from Astra import Astra
 from Detection import detector
 import cv2 as cv
 import os
-from autolab_core import BinaryImage, ColorImage, DepthImage, RgbdImage, YamlConfig, CameraIntrinsics
-
-import subprocess
-from gqcnn.grasping import RgbdImageState,FullyConvolutionalGraspingPolicyParallelJaw
+from autolab_core import ColorImage, DepthImage, RgbdImage, YamlConfig, CameraIntrinsics
+from gqcnn.grasping import RgbdImageState, FullyConvolutionalGraspingPolicyParallelJaw
 import time
 
-ImageFolder = 'ImTestPics'
-
-#initialize policy:
-config = YamlConfig(os.path.join(os.path.dirname(os.path.realpath(__file__)),"cfg/examples/fc_gqcnn_pj.yaml"))
+# Initialize policy
+config = YamlConfig(os.path.join(os.path.dirname(os.path.realpath(__file__)), "cfg/examples/fc_gqcnn_pj.yaml"))
 policy_config = config["policy"]
 policy = FullyConvolutionalGraspingPolicyParallelJaw(policy_config)
-def invokeDexNet(color,depth,segmask):
-    '''
-    for now we can probably just directly invoke it from this python script by saving color.png
-    segmask.png, and depth.npy and calling examples/policy.py.
-    eventually, this should be rebuilt as our own implementation of policy.py
-    '''
-    #segmask_file = os.path.join(ImageFolder,'segmask.png')
-    #color_file = (os.path.join(ImageFolder,'color.png'))   
-    #depth_file = (os.path.join(ImageFolder,'depth.npy'))
-    
+
+def invokeDexNet(color, depth, segmask):
+    """
+    Invokes DexNet grasping policy on a given RGB-D image with segmentation mask.
+
+    Parameters:
+        color (numpy.ndarray): Color image.
+        depth (numpy.ndarray): Depth image.
+        segmask (autolab_core.BinaryImage): Segmentation mask.
+
+    Returns:
+        action: Grasping action.
+    """
     color_im = ColorImage(color)
     depth_im = DepthImage(depth).inpaint()
 
-    rgbd_im = RgbdImage.from_color_and_depth(color_im,depth_im)
-    camera_intr = CameraIntrinsics.load(os.path.join(os.path.dirname(os.path.realpath(__file__)),"Astra/Astra_IR.intr"))
-    state = RgbdImageState(rgbd_im, camera_intr, segmask = segmask)
+    rgbd_im = RgbdImage.from_color_and_depth(color_im, depth_im)
+    camera_intr = CameraIntrinsics.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), "Astra/Astra_IR.intr"))
+    state = RgbdImageState(rgbd_im, camera_intr, segmask=segmask)
     policy_start = time.time()
     action = policy(state)
-    print('\n Planning took %.3f sec' %(time.time()-policy_start))
-    print(action.grasp.feature_vec)
-    #segmask.save(segmask_file)
-    #color_im.save(color_file)
-    #depth_im.save(depth_file)
-    
-    #command = ["python","examples/policy.py","FC-GQCNN-4.0-PJ","--fully_conv","--color_image",color_file,"--depth_image",depth_file,"--segmask",segmask_file]
-    #subprocess.run(command)
+    print('\n Planning took %.3f sec' % (time.time() - policy_start))
+    return action
 
-if __name__=="__main__":
-    #Setup camera:
+if __name__ == "__main__":
+    # Setup camera
     camera = Astra.Astra()
     camera.start()
-    
 
-
-    # Check if the folder exists
-    if not os.path.exists(ImageFolder):
-        # If the folder doesn't exist, create it
-        os.makedirs(ImageFolder)
-    
-    #define mouse click event:
+    # Define mouse click event
     posList = []
     runflag = False
-    def onMouse(event,x,y,flags,param):
+
+    def onMouse(event, x, y, flags, param):
+        """
+        Mouse click event handler.
+
+        Records mouse click positions for object detection.
+
+        Parameters:
+            event: Type of mouse event.
+            x (int): x-coordinate of the mouse click.
+            y (int): y-coordinate of the mouse click.
+            flags: Flags indicating the state of the mouse buttons.
+            param: Additional parameters.
+        """
         global posList, runflag
         if event == cv.EVENT_LBUTTONDOWN:
-            posList.append((x,y))
+            posList.append((x, y))
             runflag = True
-    #Assign mouseclick to the color and binary windows
+
+    # Assign mouse click to the color and binary windows
     cv.namedWindow('color')
-    cv.setMouseCallback('color',onMouse)
+    cv.setMouseCallback('color', onMouse)
     cv.namedWindow('binary_image')
-    cv.setMouseCallback('binary_image',onMouse)
+    cv.setMouseCallback('binary_image', onMouse)
     cv.namedWindow('depth')
-    cv.setMouseCallback('depth',onMouse)
-    #set up detector
+    cv.setMouseCallback('depth', onMouse)
+
+    # Set up object detector
     detector = detector.Detector("Detection/example_config.json")
-    #main event loop
+
+    # Main event loop
     while 1:
-        color,depth = camera.frames()
-        cv.imshow("color",color)
-        cv.imshow('depth',depth)
-        contours,full_binary_image = detector.detect_objects(color,depth)
-        cv.imshow("binary_image",full_binary_image._image_data())
+        color, depth = camera.frames()
+        cv.imshow("color", color)
+        cv.imshow('depth', depth)
+        contours, full_binary_image = detector.detect_objects(color, depth)
+        cv.imshow("binary_image", full_binary_image._image_data())
         if runflag:
-            #itemfile=open("../../franky/franky/items/Pending_Items_Camera.txt", "a")
-            #itemfile.write(str(posList[0][0]) + "," + str(posList[0][1])+"|\n")
-            #itemfile.close()
-            #STARTfile=open("../../franky/franky/items/START.txt", "a")
-            #STARTfile.write("START")
-            #STARTfile.close()
             runflag = False
             containing_contour = detector.find_contour_near_point(contours, posList[0])
             posList.pop(0)
@@ -91,5 +87,6 @@ if __name__=="__main__":
                 print("No object found")
             else:
                 single_obj_bin_im = full_binary_image.contour_mask(containing_contour)
-                invokeDexNet(color,depth,single_obj_bin_im)
+                invokeDexNet(color, depth, single_obj_bin_im)
         cv.waitKey(1)
+
