c60a933178562362b1fa3bae162d88fa85906141
nannn3 <millert9999@gmail.com>
2024-04-30 12:32:10 -0700

    Saving progress. Need to shift depth im up by ~50 px

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pdb
 import cv2 as cv
 import os
-from autolab_core import ColorImage, DepthImage, RgbdImage, YamlConfig, CameraIntrinsics
+from autolab_core import ColorImage, DepthImage, RgbdImage, YamlConfig, CameraIntrinsics, PointCloud
 from gqcnn.grasping import RgbdImageState, FullyConvolutionalGraspingPolicyParallelJaw
 import time
+import numpy as np
 
 from Astra import Astra
 from Detection import detector
@@ -27,13 +28,16 @@ def invokeDexNet(color, depth, segmask):
         action: Grasping action.
     """
     color_im = ColorImage(color)
-    depth_im = DepthImage(depth).inpaint()
-
+    depth_im = DepthImage(depth).inpaint(.50)
+    cv.imshow('inpainted',depth_im._image_data())
+    cv.waitKey(1)
     rgbd_im = RgbdImage.from_color_and_depth(color_im, depth_im)
     camera_intr = CameraIntrinsics.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), "Astra/Astra_IR.intr"))
     state = RgbdImageState(rgbd_im, camera_intr, segmask=segmask)
+    
     policy_start = time.time()
     action = policy(state)
+    
     print('\n Planning took %.3f sec' % (time.time() - policy_start))
     return action
 
@@ -49,14 +53,14 @@ def draw_grasp(action, im):
     p1 = (int(foo[0]), int(foo[1]))
     p2 = (int(foo[2]), int(foo[3]))
     depth = foo[4]
-    '''
-    Write to output file
+    
+    # Write to output file
     centroidX=action.grasp.center.vector[0]
     centroidY=action.grasp.center.vector[1]
     outfile = open("../../franky/franky/items/Items_Rot_Dep.txt","a")
     outdict={"X_0":centroidX,"Y_0":centroidY,"X_1":p1[0],"Y_1":p1[1],"X_2":p2[0],"Y_2":p2[1],"Dep":depth}
     outfile.write(str(outdict)+"\n")
-    '''
+    
     
     
     
@@ -83,25 +87,50 @@ def draw_grasp(action, im):
     return im_rec
 
 if __name__ == "__main__":
-    # Setup camera
-    camera = Astra.Astra()
+    camera_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'Astra')
+    color_intr_file = os.path.join(camera_file_path,'Astra_Color.intr')
+    ir_intr_file = os.path.join(camera_file_path,'Astra_IR.intr')
+    # Initialize the camera
+    camera = Astra.Astra(color_intr_path = color_intr_file, ir_intr_path = ir_intr_file)
     camera.start()
     pixel_input = PixelInput()
 
     # Set up object detector
     detector = detector.Detector("Detection/example_config.json")
-
+    R = np.array([
+        [-.9998,.0095,-.0196],
+        [.0112,.9957,-.0924],
+        [.0187,-.0926,-.9955]
+        ])
     # Main event loop
     while 1:
         color, depth = camera.frames()
+        point_cloud = camera.depth_to_point_cloud(depth)
+        point_cloud = camera.rotate_point_cloud(point_cloud,R) 
+        depth = camera.point_cloud_to_depth(point_cloud)
         cv.imshow("color", color)
         cv.imshow('depth', depth)
-
+        '''
+        Tape depths:
+        depth_TL = depth[136][189]
+        depth_TR = depth[92][451]
+        depth_BL = depth[331][219]
+        depth_BR = depth[270][530]
+        
+        print(f'depth at (189,136):{depth_TL}\n'
+                f'depth at (451,92):{depth_TR}\n'
+                f'depth at (219,331):{depth_BL}\n'
+                f'depth at (530,270):{depth_BR}\n'
+                )
+        '''
+       
         contours, full_binary_image = detector.detect_objects(color, depth)
         cv.imshow("binary_image", full_binary_image._image_data())
 
         x,y = pixel_input.get_pixel_coordinates()
         if x is not None and y is not None:
+            depth_at_x_y = depth[y][x]
+            print(f'depth at ({x},{y}) is {depth_at_x_y}')
             containing_contour = detector.find_contour_near_point(contours,(x,y))
             if containing_contour is None:
                 print("No object found")
@@ -110,6 +139,7 @@ if __name__ == "__main__":
                 action = invokeDexNet(color, depth, single_obj_bin_im)
                 if policy_config["vis"]["final_grasp"]:
                     im = draw_grasp(action,color)
+
                     cv.imshow("Planned grasp",im)
 
         cv.waitKey(1)
